493de3f2592e6392947d6d1adaf85059fbaf3c55
LoggingCo <ksy9527s@gamil.com>
2022-11-09 01:30:00 +0900

    style: full modal layout style
    룸 페이지 모달 레이아웃 구현 완료

--- src/components/Pages/room/Modal/Full/Chat/Chat.tsx
+++ src/components/Pages/room/Modal/Full/Chat/Chat.tsx
@@ -4,6 +4,10 @@ function FullRoomChat() {
   return (
     <Styled.Wrapper>
       <Styled.Header>채팅</Styled.Header>
+      <Styled.Contatiner></Styled.Contatiner>
+      <Styled.Form>
+        <textarea></textarea>
+      </Styled.Form>
     </Styled.Wrapper>
   );
 }
--- src/components/Pages/room/Modal/Full/Chat/Style.tsx
+++ src/components/Pages/room/Modal/Full/Chat/Style.tsx
@@ -5,10 +5,30 @@ export const Wrapper = styled.div`
   width: 25%;
   height: 100vh;
   background-color: #f6f1f0;
+  position: relative;
 `;
 
 export const Header = styled.div`
   height: 48px;
   background-color: #fff;
   ${flexCenter};
+`;
+
+export const Contatiner = styled.div`
+  height: calc(100vh - 48px - 100px);
+`;
+
+export const Form = styled.form`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+
+  & textarea {
+    width: 100%;
+    outline: none;
+    border: none;
+    resize: none;
+    height: 100px;
+    padding: 16px;
+  }
 `;
